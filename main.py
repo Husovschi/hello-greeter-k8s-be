c85433d2ea870c6019a53b8d166d223347c4de6e
Andrei Husovschi <husovschi@protonmail.com>
2023-05-16 09:09:42 +0300

    Added CORSMiddleware

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/env/{env_var}")
 async def read_env_var(env_var: str):
